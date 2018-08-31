1d039030049d6a9cc8e663cb57bfb26cda2e2191
DK\IRO <iro@sitecore.net>
2018-08-31 18:53:09 +0300

    moved logic from the item:added event to 'createItem' pipeline from 'itemProvider' group

---
+++ src/Sitecore.Support.251817/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.251817")]
+[assembly: AssemblyProduct("Sitecore.Support.251817")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.251817/SeoUrlHelperProcessor.cs
@@ -0,0 +1,41 @@
+﻿namespace Sitecore.Support.XA.Foundation.ContentValidation.Pipelines.ItemProvider.CreateItem
+{
+  using System;
+  using Sitecore.Caching;
+  using Sitecore.Data.Items;
+  using Sitecore.Data.Managers;
+  using Sitecore.Data.Templates;
+  using Sitecore.Pipelines.ItemProvider.CreateItem;
+  using Sitecore.XA.Foundation.SitecoreExtensions;
+  using Sitecore.XA.Foundation.SitecoreExtensions.Extensions;
+
+  public class SeoUrlHelperProcessor : Sitecore.Pipelines.ItemProvider.CreateItem.CreateItemProcessor
+  {
+    public override void Process(CreateItemArgs args)
+    {
+      string normalizedName = args.ItemName.GetNormalizedName();
+      Template destinationItemTemplate = TemplateManager.GetTemplate(args.TemplateId, args.Destination.Database);
+
+      if (!JobsHelper.IsPublishing() && ShouldValidate(destinationItemTemplate) && !(args.ItemName == normalizedName) && !args.Destination.Paths.FullPath.StartsWith("/sitecore/templates/", StringComparison.OrdinalIgnoreCase))
+      {
+        Item destinationItem = args.FallbackProvider.CreateItem(normalizedName, args.Destination, args.TemplateId, args.NewId, args.SecurityCheck);
+        HandleItemName(destinationItem, args.ItemName);
+        args.Result = destinationItem;
+      }
+    }
+
+    protected virtual void HandleItemName(Item destinationItem, string originalName)
+    {
+      ItemPathsCache itemPathsCache = CacheManager.GetItemPathsCache(destinationItem.Database);
+      destinationItem.Editing.BeginEdit();
+      destinationItem.Appearance.DisplayName = originalName;
+      destinationItem.Editing.EndEdit();
+      itemPathsCache.InvalidateCache(destinationItem);
+    }
+
+    protected virtual bool ShouldValidate(Template item)
+    {
+      return item.InheritsFrom(Sitecore.XA.Foundation.ContentValidation.Templates._SeoUrlValid.ID);
+    }
+  }
+}
