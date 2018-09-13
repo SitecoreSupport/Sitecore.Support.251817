0c5f6a5f5ad7aeddee1e6ed3c8521368469c1fcc
DK\IRO <iro@sitecore.net>
2018-09-13 11:49:36 +0300

    changed type of args parameter to avoid issue during the publishing

--- src/Sitecore.Support.251817/SeoUrlHelperProcessor.cs
+++ src/Sitecore.Support.251817/SeoUrlHelperProcessor.cs
@@ -5,22 +5,30 @@
   using Sitecore.Data.Items;
   using Sitecore.Data.Managers;
   using Sitecore.Data.Templates;
+  using Sitecore.Pipelines;
   using Sitecore.Pipelines.ItemProvider.CreateItem;
   using Sitecore.XA.Foundation.SitecoreExtensions;
   using Sitecore.XA.Foundation.SitecoreExtensions.Extensions;
 
-  public class SeoUrlHelperProcessor : Sitecore.Pipelines.ItemProvider.CreateItem.CreateItemProcessor
-  {
-    public override void Process(CreateItemArgs args)
+  public class SeoUrlHelperProcessor
+  { 
+    public  void Process(ProviderResultPipelineArgs<Item, ItemProviderBase> args)
     {
-      string normalizedName = args.ItemName.GetNormalizedName();
-      Template destinationItemTemplate = TemplateManager.GetTemplate(args.TemplateId, args.Destination.Database);
-
-      if (!JobsHelper.IsPublishing() && ShouldValidate(destinationItemTemplate) && !(args.ItemName == normalizedName) && !args.Destination.Paths.FullPath.StartsWith("/sitecore/templates/", StringComparison.OrdinalIgnoreCase))
+      if (!JobsHelper.IsPublishing())
       {
-        Item destinationItem = args.FallbackProvider.CreateItem(normalizedName, args.Destination, args.TemplateId, args.NewId, args.SecurityCheck);
-        HandleItemName(destinationItem, args.ItemName);
-        args.Result = destinationItem;
+        CreateItemArgs createItemArgs = args as CreateItemArgs;
+        if (args != null)
+        {
+          string normalizedName = createItemArgs.ItemName.GetNormalizedName();
+          Template destinationItemTemplate = TemplateManager.GetTemplate(createItemArgs.TemplateId, createItemArgs.Destination.Database);
+
+          if (ShouldValidate(destinationItemTemplate) && !(createItemArgs.ItemName == normalizedName) && !createItemArgs.Destination.Paths.FullPath.StartsWith("/sitecore/templates/", StringComparison.OrdinalIgnoreCase))
+          {
+            Item destinationItem = args.FallbackProvider.CreateItem(normalizedName, createItemArgs.Destination, createItemArgs.TemplateId, createItemArgs.NewId, createItemArgs.SecurityCheck);
+            HandleItemName(destinationItem, createItemArgs.ItemName);
+            args.Result = destinationItem;
+          }
+        }
       }
     }
 
